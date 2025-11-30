fa84db5134ae06d754272fa43466b26c8f5880e9
suraj-nis <surslayeryt@gmail.com>
2025-11-29 19:49:04 -0500

    Initial Commit

---
+++ MealSaver/MealSaverApp.swift
@@ -0,0 +1,17 @@
+//
+//  MealSaverApp.swift
+//  MealSaver
+//
+//  Created by Suraj Nistala on 11/29/25.
+//
+
+import SwiftUI
+
+@main
+struct MealSaverApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ MealSaverTests/MealSaverTests.swift
@@ -0,0 +1,17 @@
+//
+//  MealSaverTests.swift
+//  MealSaverTests
+//
+//  Created by Suraj Nistala on 11/29/25.
+//
+
+import Testing
+@testable import MealSaver
+
+struct MealSaverTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
